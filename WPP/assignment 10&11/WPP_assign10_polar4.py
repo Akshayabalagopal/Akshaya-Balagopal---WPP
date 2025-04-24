9c38fd635bd667ae4dadd991ee6d9855ee89b3f0
Akshayabalagopal <i24ai001@aid.svnit.ac.in>
2025-04-24 16:42:21 +0530

    Update WPP_assign10_polar4.py

--- WPP/assignment 10&11/WPP_assign10_polar4.py
+++ WPP/assignment 10&11/WPP_assign10_polar4.py
@@ -1,3 +1,6 @@
+'''4. Take N (N >= 10) random 2-dimensional points represented in cartesian coordinate space.
+Store them in a numpy array. Convert them to polar coordinates.'''
+
 import numpy as np
 
 def cartesian_to_polar(N):
